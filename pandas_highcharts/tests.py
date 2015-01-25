ec366f642e9f6c49b8d68e1e5006cc584eecd1d5
Guillaume Thomas <guillaume.thomas642@gmail.com>
2015-01-25 16:03:48 +0100

    removed README.md added tests.py

---
+++ pandas_highcharts/tests.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+
+from core import serialize
+import pandas
+
+
+df = pandas.DataFrame([{"a": 1, "b": 2, "c": 3}, {"a": 2, "b": 4, "c": 6}])
+
+
+def test_type():
+    assert type(serialize(df, render_to="chart")) is str
+    assert type(serialize(df, x="a", render_to="chart")) is str
+    assert type(serialize(df, x="a", y=("b",), render_to="chart")) is str
+    assert type(serialize(df, figsize=(100, 100), render_to="chart")) is str
