67d7eb9d3838a4e2db8ec6545a4ddc7d235c6e60
Damien Garaud <damien.garaud@gmail.com>
2015-02-26 23:48:38 +0100

    Allow to have the zoom plotting feature closes #1

--- pandas_highcharts/tests.py
+++ pandas_highcharts/tests.py
@@ -10,5 +10,6 @@ df = pandas.DataFrame([{"a": 1, "b": 2, "c": 3}, {"a": 2, "b": 4, "c": 6}])
 def test_type():
     assert type(serialize(df, render_to="chart")) is str
     assert type(serialize(df, x="a", render_to="chart")) is str
+    assert type(serialize(df, x="a", zoom="x", render_to="chart")) is str
     assert type(serialize(df, x="a", y=("b",), render_to="chart")) is str
     assert type(serialize(df, figsize=(100, 100), render_to="chart")) is str
