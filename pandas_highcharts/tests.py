7b9a942ca8f4ad9910fd0e169a11d5eaf6c1cf0b
Guillaume Thomas <guillaume.thomas642@gmail.com>
2015-03-08 08:45:57 +0100

    tests comptabible with python 2.6

--- pandas_highcharts/tests.py
+++ pandas_highcharts/tests.py
@@ -13,17 +13,17 @@ df = pandas.DataFrame([
 
 class CoreTest(TestCase):
     def test_type(self):
-        self.assertIsInstance(serialize(df, render_to="chart"), str)
+        self.assertEqual(type(serialize(df, render_to="chart")), str)
         obj = serialize(df, render_to="chart", output_type="json")
-        self.assertIsInstance(obj, dict)
+        self.assertEqual(type(obj), dict)
         obj = serialize(df, render_to="chart", output_type="json", zoom="xy")
         self.assertIn("chart", obj)
-        self.assertIsInstance(obj["chart"], dict)
+        self.assertEqual(type(obj["chart"]), dict)
         self.assertIn("zoomType", obj["chart"])
         self.assertRaises(ValueError, serialize, df, **{"render_to": "chart", "zoom": "z"})
         obj = serialize(df, render_to="chart", output_type="json", kind="bar")
         self.assertIn("chart", obj)
-        self.assertIsInstance(obj["chart"], dict)
+        self.assertEqual(type(obj["chart"]), dict)
         self.assertEqual(obj["chart"].get("type"), "column")
         self.assertRaises(ValueError, serialize, df, **{"render_to": "chart", "kind": "z"})
         obj = serialize(df, render_to="chart", output_type="json", secondary_y="a")
