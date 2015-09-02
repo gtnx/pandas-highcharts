106af17d2cb8de213f8b7e56b14c3039a5b7d529
Guillaume Thomas <guillaume.thomas642@gmail.com>
2015-09-02 23:09:15 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='pandas-highcharts',
-    version='0.4.6',
+    version='0.5.1',
     author='Guillaume Thomas',
     author_email='guillaume.thomas642@gmail.com',
     license='LICENSE',
