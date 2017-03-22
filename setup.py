bf449b7db8b6966bcf95a0280bf2e4518f3e2419
Guillaume Thomas <guillaume.thomas642@gmail.com>
2017-03-22 13:20:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.5.2',
     author='Guillaume Thomas',
     author_email='guillaume.thomas642@gmail.com',
-    license='LICENSE',
+    license='MIT',
     description='pandas-highcharts is a Python package which allows you to easily build Highcharts plots with pandas.DataFrame objects.',
     url='https://github.com/gtnx/pandas-highcharts',
     install_requires=[line.strip("\n") for line in open("requirements.txt", "r").readlines()],
